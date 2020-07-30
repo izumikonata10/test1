672c9384b3ed7a986cdc6f748c6772129239ee56
izumi <2444639628@qq.com>
2020-07-30 14:50:53 +0800

    搭建完成v1.1

--- src/main/java/org/izumi/IzumiApplication.java
+++ src/main/java/org/izumi/IzumiApplication.java
@@ -3,6 +3,7 @@ package org.izumi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Song on 2017/2/15.
  * 官方示例工程中的测试代码
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.izumi")
 @MapperScan(basePackages = "org.izumi.dao")
+@Component
 public class IzumiApplication {
 
     public static void main(String[] args) throws Exception {
--- src/main/java/org/izumi/controller/IndexController.java
+++ src/main/java/org/izumi/controller/IndexController.java
@@ -1,8 +1,12 @@
 package org.izumi.controller;
 
+import org.izumi.dao.ZhaoMapper;
+import org.izumi.model.Zhao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -13,8 +17,16 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @Autowired(required = false)
+    ZhaoMapper zhaoMapper;
+
+    @RequestMapping("/test")
     public String home() {
-        return "/index";
+        Zhao z=new Zhao();
+        z.setzName("zz");
+        z.setzId(22);
+        z.setzMoney(22);
+        zhaoMapper.insert(z);
+        return "index";
     }
 }
