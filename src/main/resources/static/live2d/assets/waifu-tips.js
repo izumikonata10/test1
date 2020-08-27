091d9256145d67e332aaaeeb6e2ad7a48cbd0bb9
izumi <2444639628@qq.com>
2020-08-27 08:53:26 +0800

    izumi_v1.22 live2d样式修改  按钮bug修复

--- src/main/resources/static/live2d/assets/waifu-tips.js
+++ src/main/resources/static/live2d/assets/waifu-tips.js
@@ -41,7 +41,7 @@ live2d_settings['canTurnToAboutPage']   = true;         // 显示 跳转关于�
 // 模型切换模式
 live2d_settings['modelStorage']         = true;         // 记录 ID (刷新后恢复)，可选 true(真), false(假)
 live2d_settings['modelRandMode']        = 'switch';     // 模型切换，可选 'rand'(随机), 'switch'(顺序)
-live2d_settings['modelTexturesRandMode']= 'switch';       // 材质切换，可选 'rand'(随机), 'switch'(顺序)
+live2d_settings['modelTexturesRandMode']= 'rand';       // 材质切换，可选 'rand'(随机), 'switch'(顺序)
 
 // 提示消息选项
 live2d_settings['showHitokoto']         = true;         // 显示一言
@@ -136,7 +136,8 @@ function initModel(waifuPath, type) {
     $(".waifu-tips").height(live2d_settings.waifuTipsSize[1]);
     $(".waifu-tips").css("top",live2d_settings.waifuToolTop);
     $(".waifu-tips").css("font-size",live2d_settings.waifuFontSize);
-    $(".waifu-tool").css("font-size",live2d_settings.waifuToolFont);
+    // $(".waifu-tool").css("font-size",live2d_settings.waifuToolFont);
+    $(".waifu-tool").css("font-size","10px");
     $(".waifu-tool span").css("line-height",live2d_settings.waifuToolLine);
     
     if (live2d_settings.waifuEdgeSide[0] == 'left') $(".waifu").css("left",live2d_settings.waifuEdgeSide[1]+'px');
