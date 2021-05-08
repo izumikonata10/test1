4666d7f15742a98c2bd5628b594cecc3a85d0695
izumi <2444639628@qq.com>
2021-05-08 08:49:41 +0800

    izumi_v1.341 新增图片压缩方法和test

--- src/main/java/org/izumi/utils/ImageCompressUtil.java
+++ src/main/java/org/izumi/utils/ImageCompressUtil.java
@@ -1,5 +1,7 @@
 package org.izumi.utils;
 
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,12 +10,10 @@ import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @auther izumi
@@ -67,5 +67,69 @@ public class ImageCompressUtil {
             logger.debug("图片转换后大小"+file.length()+"字节");
             return file;
         }
+    }
+
+    /**
+     * 指定图片宽度和高度和压缩比例对图片进行压缩
+     *
+     * @param srcfile 源图片文件
+     * @param imgdist 目标图片地址
+     */
+    public static void reduceImg(File srcfile, String imgdist) {
+        try {
+            // 检查图片文件是否存在
+            if (!srcfile.exists()) {
+                System.out.println("文件不存在");
+            }
+            int[] results = getImgWidthHeight(srcfile);
+
+            int widthdist = results[0];
+            int heightdist = results[1];
+            // 开始读取文件并进行压缩
+            Image src = ImageIO.read(srcfile);
+
+            // 构造一个类型为预定义图像类型之一的 BufferedImage
+            BufferedImage tag = new BufferedImage( widthdist,  heightdist, BufferedImage.TYPE_INT_RGB);
+
+            // 这边是压缩的模式设置
+            tag.getGraphics().drawImage(src.getScaledInstance(widthdist, heightdist, Image.SCALE_SMOOTH), 0, 0, null);
+
+            //创建文件输出流
+            FileOutputStream out = new FileOutputStream(imgdist);
+            //将图片按JPEG压缩，保存到out中
+            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+            encoder.encode(tag);
+            //关闭文件输出流
+            out.close();
+        } catch (Exception ef) {
+            ef.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取图片宽度和高度
+     *
+     * @param file 图片路径
+     * @return 返回图片的宽度
+     */
+    public static int[] getImgWidthHeight(File file) {
+        InputStream is = null;
+        BufferedImage src = null;
+        int result[] = { 0, 0 };
+        try {
+            // 获得文件输入流
+            is = new FileInputStream(file);
+            // 从流里将图片写入缓冲图片区
+            src = ImageIO.read(is);
+            // 得到源图片宽
+            result[0] =src.getWidth(null);
+            // 得到源图片高
+            result[1] =src.getHeight(null);
+            is.close();  //关闭输入流
+        } catch (Exception ef) {
+            ef.printStackTrace();
+        }
+
+        return result;
     }
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,3 +1,6 @@
+import org.izumi.utils.ImageCompressUtil;
+
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.UUID;
 
@@ -6,10 +9,18 @@ import java.util.UUID;
  * @date 2020-08-22 09:28
  */
 public class test {
-    public static void main(String[] args) {
-        double a= 123.123123123;
-        DecimalFormat df = new DecimalFormat("#.00");
-        System.out.println(df.format(a));
+    public static void main(String[] args) throws Exception{
+//        double a= 123.123123123;
+//        DecimalFormat df = new DecimalFormat("#.00");
+//        System.out.println(df.format(a));
+
+        float a = (float) 0.5;
+        File file = new File("D:\\User\\Java\\Desktop\\年会表彰照片");
+        File[] files = file.listFiles();
+        for(File f : files){
+//            ImageCompressUtil.compressPictureByQality(f,a);
+            ImageCompressUtil.reduceImg(f,f.getPath());
+        }
     }
 
 }
